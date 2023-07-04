b55919a249a8147b6cf82b219efca5601544b2bd
Fabrizio Ruocco <ruoccofabrizio@gmail.com>
2023-07-04 08:58:31 +0200

    added basic logging for qna

--- backend/utilities/LLMHelper.py
+++ backend/utilities/LLMHelper.py
@@ -22,6 +22,8 @@ from langchain.document_loaders.base import BaseLoader
 from langchain.document_loaders import TextLoader
 from langchain.chat_models import AzureChatOpenAI
 from langchain.schema import AIMessage, HumanMessage, SystemMessage
+from langchain.callbacks import get_openai_callback
+from opencensus.ext.azure.log_exporter import AzureLogHandler
 
 from .formrecognizer import AzureFormRecognizerClient
 from .azureblobstorage import AzureBlobStorageClient
@@ -33,9 +35,20 @@ import urllib
 
 from fake_useragent import UserAgent
 
+# Setting logging
+load_dotenv()
+logger = logging.getLogger(__name__)
+instrumentation_key = (
+    f"InstrumentationKey={os.getenv('APPINSIGHTS_INSTRUMENTATIONKEY')}"
+)
+logger.addHandler(AzureLogHandler(connection_string=instrumentation_key))
+logger.setLevel(logging.INFO)
+
+
 class LLMHelper:
-    def __init__(self,
-        document_loaders : BaseLoader = None, 
+    def __init__(
+        self,
+        document_loaders: BaseLoader = None,
         text_splitter: TextSplitter = None,
         embeddings: OpenAIEmbeddings = None,
         llm: AzureOpenAI = None,
@@ -45,15 +58,17 @@ class LLMHelper:
         vector_store: VectorStore = None,
         k: int = None,
         pdf_parser: AzureFormRecognizerClient = None,
-        blob_client: AzureBlobStorageClient = None):
-
-        os.environ["OPENAI_API_BASE"] = f"https://{os.getenv('AZURE_OPENAI_RESOURCE')}.openai.azure.com/"
+        blob_client: AzureBlobStorageClient = None,
+    ):
+        os.environ[
+            "OPENAI_API_BASE"
+        ] = f"https://{os.getenv('AZURE_OPENAI_RESOURCE')}.openai.azure.com/"
         os.environ["OPENAI_API_KEY"] = os.getenv("AZURE_OPENAI_KEY")
         os.environ["OPENAI_API_VERSION"] = os.getenv("AZURE_OPENAI_API_VERSION")
 
-        load_dotenv('..\.env')
+        load_dotenv("..\.env")
         openai.api_type = "azure"
-        openai.api_base = os.getenv('OPENAI_API_BASE')
+        openai.api_base = os.getenv("OPENAI_API_BASE")
         openai.api_version = os.getenv("AZURE_OPENAI_API_VERSION")
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -61,31 +76,89 @@ class LLMHelper:
         self.api_base = openai.api_base
         self.api_version = openai.api_version
         self.index_name: str = os.getenv("AZURE_SEARCH_INDEX")
-        self.model: str = os.getenv('OPENAI_EMBEDDINGS_ENGINE_DOC', "text-embedding-ada-002")
+        self.model: str = os.getenv(
+            "OPENAI_EMBEDDINGS_ENGINE_DOC", "text-embedding-ada-002"
+        )
         self.deployment_name: str = os.getenv("AZURE_OPENAI_MODEL", "gpt-35-turbo")
         self.deployment_type: str = os.getenv("OPENAI_DEPLOYMENT_TYPE", "Chat")
-        self.temperature: float = float(os.getenv("AZURE_OPENAI_TEMPERATURE", 0.7)) if temperature is None else temperature
-        self.max_tokens: int = int(os.getenv("AZURE_OPENAI_MAX_TOKENS", -1)) if max_tokens is None else max_tokens
-        self.prompt = PROMPT if custom_prompt == '' else PromptTemplate(template=custom_prompt, input_variables=["summaries", "question"])
+        self.temperature: float = (
+            float(os.getenv("AZURE_OPENAI_TEMPERATURE", 0.7))
+            if temperature is None
+            else temperature
+        )
+        self.max_tokens: int = (
+            int(os.getenv("AZURE_OPENAI_MAX_TOKENS", -1))
+            if max_tokens is None
+            else max_tokens
+        )
+        self.prompt = (
+            PROMPT
+            if custom_prompt == ""
+            else PromptTemplate(
+                template=custom_prompt, input_variables=["summaries", "question"]
+            )
+        )
 
         # Azure Search settings
-        self.vector_store_address: str = os.getenv('AZURE_SEARCH_SERVICE')
-        self.vector_store_password: str = os.getenv('AZURE_SEARCH_KEY')
+        self.vector_store_address: str = os.getenv("AZURE_SEARCH_SERVICE")
+        self.vector_store_password: str = os.getenv("AZURE_SEARCH_KEY")
 
-        self.chunk_size = int(os.getenv('CHUNK_SIZE', 500))
-        self.chunk_overlap = int(os.getenv('CHUNK_OVERLAP', 100))
-        self.document_loaders: BaseLoader = WebBaseLoader if document_loaders is None else document_loaders
-        self.text_splitter: TextSplitter = TokenTextSplitter(chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap) if text_splitter is None else text_splitter
-        self.embeddings: OpenAIEmbeddings = OpenAIEmbeddings(model=self.model, chunk_size=1) if embeddings is None else embeddings
+        self.chunk_size = int(os.getenv("CHUNK_SIZE", 500))
+        self.chunk_overlap = int(os.getenv("CHUNK_OVERLAP", 100))
+        self.document_loaders: BaseLoader = (
+            WebBaseLoader if document_loaders is None else document_loaders
+        )
+        self.text_splitter: TextSplitter = (
+            TokenTextSplitter(
+                chunk_size=self.chunk_size, chunk_overlap=self.chunk_overlap
+            )
+            if text_splitter is None
+            else text_splitter
+        )
+        self.embeddings: OpenAIEmbeddings = (
+            OpenAIEmbeddings(model=self.model, chunk_size=1)
+            if embeddings is None
+            else embeddings
+        )
         if self.deployment_type == "Chat":
-            self.llm: AzureChatOpenAI = AzureChatOpenAI(deployment_name=self.deployment_name, temperature=self.temperature, openai_api_version=self.api_version, max_tokens=self.max_tokens if self.max_tokens != -1 else None) if llm is None else llm
+            self.llm: AzureChatOpenAI = (
+                AzureChatOpenAI(
+                    deployment_name=self.deployment_name,
+                    temperature=self.temperature,
+                    openai_api_version=self.api_version,
+                    max_tokens=self.max_tokens if self.max_tokens != -1 else None,
+                )
+                if llm is None
+                else llm
+            )
         else:
-            self.llm: AzureOpenAI = AzureOpenAI(deployment_name=self.deployment_name, temperature=self.temperature, max_tokens=self.max_tokens) if llm is None else llm
-        self.vector_store: VectorStore = AzureSearch(azure_cognitive_search_name=self.vector_store_address, azure_cognitive_search_key=self.vector_store_password, index_name=self.index_name, embedding_function=self.embeddings.embed_query) if vector_store is None else vector_store
-        self.k : int = 3 if k is None else k
+            self.llm: AzureOpenAI = (
+                AzureOpenAI(
+                    deployment_name=self.deployment_name,
+                    temperature=self.temperature,
+                    max_tokens=self.max_tokens,
+                )
+                if llm is None
+                else llm
+            )
+        self.vector_store: VectorStore = (
+            AzureSearch(
+                azure_cognitive_search_name=self.vector_store_address,
+                azure_cognitive_search_key=self.vector_store_password,
+                index_name=self.index_name,
+                embedding_function=self.embeddings.embed_query,
+            )
+            if vector_store is None
+            else vector_store
+        )
+        self.k: int = 3 if k is None else k
 
-        self.pdf_parser : AzureFormRecognizerClient = AzureFormRecognizerClient() if pdf_parser is None else pdf_parser
-        self.blob_client: AzureBlobStorageClient = AzureBlobStorageClient() if blob_client is None else blob_client
+        self.pdf_parser: AzureFormRecognizerClient = (
+            AzureFormRecognizerClient() if pdf_parser is None else pdf_parser
+        )
+        self.blob_client: AzureBlobStorageClient = (
+            AzureBlobStorageClient() if blob_client is None else blob_client
+        )
 
         self.user_agent: UserAgent() = UserAgent()
         self.user_agent.random
@@ -93,46 +166,63 @@ class LLMHelper:
     def add_embeddings_lc(self, source_url):
         try:
             documents = self.document_loaders(source_url).load()
-            
+
             # Convert to UTF-8 encoding for non-ascii text
-            for(document) in documents:
+            for document in documents:
                 try:
-                    if document.page_content.encode("iso-8859-1") == document.page_content.encode("latin-1"):
-                        document.page_content = document.page_content.encode("iso-8859-1").decode("utf-8", errors="ignore")
+                    if document.page_content.encode(
+                        "iso-8859-1"
+                    ) == document.page_content.encode("latin-1"):
+                        document.page_content = document.page_content.encode(
+                            "iso-8859-1"
+                        ).decode("utf-8", errors="ignore")
                 except:
                     pass
-                
+
             docs = self.text_splitter.split_documents(documents)
-            
+
             # Remove half non-ascii character from start/end of doc content (langchain TokenTextSplitter may split a non-ascii character in half)
-            pattern = re.compile(r'[\x00-\x1f\x7f\u0080-\u00a0\u2000-\u3000\ufff0-\uffff]')
-            for(doc) in docs:
-                doc.page_content = re.sub(pattern, '', doc.page_content)
-                if doc.page_content == '':
+            pattern = re.compile(
+                r"[\x00-\x1f\x7f\u0080-\u00a0\u2000-\u3000\ufff0-\uffff]"
+            )
+            for doc in docs:
+                doc.page_content = re.sub(pattern, "", doc.page_content)
+                if doc.page_content == "":
                     docs.remove(doc)
-            
+
             keys = []
             for i, doc in enumerate(docs):
                 # Create a unique key for the document
-                source_url = source_url.split('?')[0]
-                filename = "/".join(source_url.split('/')[4:])
-                hash_key = hashlib.sha1(f"{source_url}_{i}".encode('utf-8')).hexdigest()
+                source_url = source_url.split("?")[0]
+                filename = "/".join(source_url.split("/")[4:])
+                hash_key = hashlib.sha1(f"{source_url}_{i}".encode("utf-8")).hexdigest()
                 hash_key = f"doc:{self.index_name}:{hash_key}"
                 keys.append(hash_key)
-                doc.metadata = {"source": f"[{source_url}]({source_url}_SAS_TOKEN_PLACEHOLDER_)" , "chunk": i, "key": hash_key, "filename": filename}
+                doc.metadata = {
+                    "source": f"[{source_url}]({source_url}_SAS_TOKEN_PLACEHOLDER_)",
+                    "chunk": i,
+                    "key": hash_key,
+                    "filename": filename,
+                }
             self.vector_store.add_documents(documents=docs, keys=keys)
-            
+
         except Exception as e:
             logging.error(f"Error adding embeddings for {source_url}: {e}")
             raise e
 
-    def convert_file_and_add_embeddings(self, source_url, filename, enable_translation=False):
+    def convert_file_and_add_embeddings(
+        self, source_url, filename, enable_translation=False
+    ):
         # Extract the text from the file
         text = self.pdf_parser.analyze_read(source_url)
 
         # Upload the text to Azure Blob Storage
         converted_filename = f"converted/{filename}.txt"
-        source_url = self.blob_client.upload_file("\n".join(text), f"converted/{filename}.txt", content_type='text/plain; charset=utf-8')
+        source_url = self.blob_client.upload_file(
+            "\n".join(text),
+            f"converted/{filename}.txt",
+            content_type="text/plain; charset=utf-8",
+        )
 
         print(f"Converted file uploaded to {source_url} with filename {filename}")
         # Update the metadata to indicate that the file has been converted
@@ -143,75 +233,108 @@ class LLMHelper:
         return converted_filename
 
     def get_all_documents(self, k: int = None):
-        result = self.vector_store.similarity_search(query="*", k= k if k else self.k)
-        return pd.DataFrame(list(map(lambda x: {
-                'key': x.metadata['key'],
-                'filename': x.metadata['filename'],
-                'source': urllib.parse.unquote(x.metadata['source']), 
-                'content': x.page_content, 
-                'metadata' : x.metadata,
-                }, result)))
+        result = self.vector_store.similarity_search(query="*", k=k if k else self.k)
+        return pd.DataFrame(
+            list(
+                map(
+                    lambda x: {
+                        "key": x.metadata["key"],
+                        "filename": x.metadata["filename"],
+                        "source": urllib.parse.unquote(x.metadata["source"]),
+                        "content": x.page_content,
+                        "metadata": x.metadata,
+                    },
+                    result,
+                )
+            )
+        )
 
     def get_answer_using_langchain(self, question, chat_history):
-        question_generator = LLMChain(llm=self.llm, prompt=CONDENSE_QUESTION_PROMPT, verbose=False)
-        doc_chain = load_qa_with_sources_chain(self.llm, chain_type="stuff", verbose=True, prompt=self.prompt)
+        question_generator = LLMChain(
+            llm=self.llm, prompt=CONDENSE_QUESTION_PROMPT, verbose=True
+        )
+        doc_chain = load_qa_with_sources_chain(
+            self.llm, chain_type="stuff", verbose=True, prompt=self.prompt
+        )
         chain = ConversationalRetrievalChain(
             retriever=self.vector_store.as_retriever(),
             question_generator=question_generator,
             combine_docs_chain=doc_chain,
             return_source_documents=True,
             return_generated_question=True,
-            # top_k_docs_for_context= self.k
         )
-                
-        result = chain({"question": question, "chat_history": chat_history})
+
+        with get_openai_callback() as cb:
+            result = chain({"question": question, "chat_history": chat_history})
+
+        properties = {
+            "custom_dimensions": {
+                "question": question,
+                "chat_history": chat_history,
+                "generated_question": result["generated_question"],
+                "source_documents": result["source_documents"],
+                "Total_Tokens": cb.total_tokens,
+                "Prompt_Tokens": cb.prompt_tokens,
+                "Completion_Tokens": cb.completion_tokens,
+            }
+        }
+        logger.info(f"ConversationalRetrievalChain", extra=properties)
+
         container_sas = self.blob_client.get_container_sas()
-        
+
         # TODO: Need to replace this with proper reference handling
-        answer = result['answer'].split('SOURCES:')[0].split('Sources:')[0].split('SOURCE:')[0].split('Source:')[0]
-        print(result)
+        answer = (
+            result["answer"]
+            .split("SOURCES:")[0]
+            .split("Sources:")[0]
+            .split("SOURCE:")[0]
+            .split("Source:")[0]
+        )
 
         messages = [
             {
                 "role": "tool",
-                "content": {
-                    "citations": [],
-                    "intent": result['generated_question']
-                },
-                "end_turn": False
+                "content": {"citations": [], "intent": result["generated_question"]},
+                "end_turn": False,
             },
-            {
-                "role": "assistant",
-                "content": answer + "[doc1]",
-                "end_turn": True
-            }
+            {"role": "assistant", "content": answer, "end_turn": True},
         ]
-        
-        for idx, doc in enumerate(result['source_documents']):
-            messages[0]["content"]["citations"].append({
-                "content": "https://www.microsoft.com\n" + doc.page_content,
-                "id": idx,
-                "chunk_id": doc.metadata['chunk'],
-                "title": doc.metadata['filename'],
-                "filepath": doc.metadata['filename'],
-                "url": doc.metadata['source'].replace('_SAS_TOKEN_PLACEHOLDER_', container_sas),
-                "metadata": doc.metadata
-            })
-            
+
+        for idx, doc in enumerate(result["source_documents"]):
+            messages[0]["content"]["citations"].append(
+                {
+                    "content": doc.page_content,
+                    "id": idx,
+                    "chunk_id": doc.metadata["chunk"],
+                    "title": doc.metadata["filename"],
+                    "filepath": doc.metadata["filename"],
+                    "url": doc.metadata["source"].replace(
+                        "_SAS_TOKEN_PLACEHOLDER_", container_sas
+                    ),
+                    "metadata": doc.metadata,
+                }
+            )
+
         # everything in content needs to be stringified to work with Azure BYOD frontend
         messages[0]["content"] = json.dumps(messages[0]["content"])
         return messages
 
     def get_embeddings_model(self):
-        OPENAI_EMBEDDINGS_ENGINE_DOC = os.getenv('AZURE_OPENAI_EMBEDDING_MODEL', os.getenv('OPENAI_EMBEDDINGS_ENGINE_DOC', 'text-embedding-ada-002'))  
-        OPENAI_EMBEDDINGS_ENGINE_QUERY = os.getenv('AZURE_OPENAI_EMBEDDING_MODEL', os.getenv('OPENAI_EMBEDDINGS_ENGINE_QUERY', 'text-embedding-ada-002'))
+        OPENAI_EMBEDDINGS_ENGINE_DOC = os.getenv(
+            "AZURE_OPENAI_EMBEDDING_MODEL",
+            os.getenv("OPENAI_EMBEDDINGS_ENGINE_DOC", "text-embedding-ada-002"),
+        )
+        OPENAI_EMBEDDINGS_ENGINE_QUERY = os.getenv(
+            "AZURE_OPENAI_EMBEDDING_MODEL",
+            os.getenv("OPENAI_EMBEDDINGS_ENGINE_QUERY", "text-embedding-ada-002"),
+        )
         return {
             "doc": OPENAI_EMBEDDINGS_ENGINE_DOC,
-            "query": OPENAI_EMBEDDINGS_ENGINE_QUERY
+            "query": OPENAI_EMBEDDINGS_ENGINE_QUERY,
         }
 
     def get_completion(self, prompt, **kwargs):
-        if self.deployment_type == 'Chat':
+        if self.deployment_type == "Chat":
             return self.llm([HumanMessage(content=prompt)]).content
         else:
             return self.llm(prompt)
