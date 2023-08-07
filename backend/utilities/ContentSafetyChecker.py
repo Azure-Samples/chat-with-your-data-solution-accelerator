8dfae5f3d393686d7e196c1a27cb5ea525655d30
Fabrizio Ruocco <ruoccofabrizio@gmail.com>
2023-08-07 16:17:43 +0200

    fixed ContentSafetyChecker

--- backend/utilities/ContentSafetyChecker.py
+++ backend/utilities/ContentSafetyChecker.py
@@ -5,17 +5,17 @@ from azure.ai.contentsafety.models import AnalyzeTextOptions
 from .EnvHelper import EnvHelper
 
 class ContentSafetyChecker:
-    def __init__(self, endpoint, key):
+    def __init__(self):
         env_helper = EnvHelper()
         self.content_safety_client = ContentSafetyClient(env_helper.AZURE_CONTENT_SAFETY_ENDPOINT, AzureKeyCredential(env_helper.AZURE_CONTENT_SAFETY_KEY))
 
     def validate_input_and_replace_if_harmful(self, text):
         response_template = f"Unfortunately, I am not able to process your question, as I have detected sensitive content that I am not allowed to process. This might be a mistake, so please try rephrasing your question."
-        text = self._filter_text_and_replace(text)
+        return self._filter_text_and_replace(text, response_template)
         
     def validate_output_and_replace_if_harmful(self, text):
         response_template = f"Unfortunately, I have detected sensitive content in my answer, which I am not allowed to show you. This might be a mistake, so please try again and maybe rephrase your question."
-        text = self._filter_text_and_replace(text)
+        return self._filter_text_and_replace(text, response_template)
     
     def _filter_text_and_replace(self, text, response_template):
         request = AnalyzeTextOptions(text=text)
@@ -31,8 +31,8 @@ class ContentSafetyChecker:
             raise
 
         filtered_text = text
-
-        if response.hate_result or response.self_harm_result or response.sexual_result or response.violence_result:
+    
+        if response.hate_result.severity > 0 or response.self_harm_result.severity > 0 or response.sexual_result.severity > 0 or response.violence_result.severity > 0:
             filtered_text = response_template
        
         return filtered_text
