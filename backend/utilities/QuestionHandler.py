a403d25c05402994f602caa24e8a8600d96408f2
ChenxiJiang333 <v-chenjiang@microsoft.com>
2024-01-03 15:49:13 +0800

    Update openai to v1

--- backend/utilities/QuestionHandler.py
+++ backend/utilities/QuestionHandler.py
@@ -1,5 +1,4 @@
 import os
-import openai
 import logging
 import re
 import json
--- backend/utilities/helpers/LLMHelper.py
+++ backend/utilities/helpers/LLMHelper.py
@@ -1,7 +1,7 @@
-import openai
+from openai import AzureOpenAI
 from typing import List
 from langchain.chat_models import AzureChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import AzureOpenAIEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from .EnvHelper import EnvHelper
 
@@ -9,29 +9,25 @@ class LLMHelper:
     def __init__(self):
         env_helper: EnvHelper = EnvHelper()
 
-        # Configure OpenAI API
-        openai.api_type = env_helper.OPENAI_API_TYPE
-        openai.api_version = env_helper.AZURE_OPENAI_API_VERSION
-        openai.api_base = env_helper.OPENAI_API_BASE
-        openai.api_key = env_helper.OPENAI_API_KEY
+        self.openai_client = AzureOpenAI(azure_endpoint=env_helper.OPENAI_API_BASE, api_version=env_helper.AZURE_OPENAI_API_VERSION, api_key=env_helper.OPENAI_API_KEY)
         
         self.llm_model = env_helper.AZURE_OPENAI_MODEL
         self.llm_max_tokens = env_helper.AZURE_OPENAI_MAX_TOKENS if env_helper.AZURE_OPENAI_MAX_TOKENS != '' else None
         self.embedding_model = env_helper.AZURE_OPENAI_EMBEDDING_MODEL
                     
     def get_llm(self):
-        return AzureChatOpenAI(deployment_name=self.llm_model, temperature=0, max_tokens=self.llm_max_tokens, openai_api_version=openai.api_version)
+        return AzureChatOpenAI(deployment_name=self.llm_model, temperature=0, max_tokens=self.llm_max_tokens, openai_api_version=self.openai_client._api_version)
     
     # TODO: This needs to have a custom callback to stream back to the UI
     def get_streaming_llm(self):
         return AzureChatOpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler], deployment_name=self.llm_model, temperature=0, 
-                               max_tokens=self.llm_max_tokens, openai_api_version=openai.api_version)
+                               max_tokens=self.llm_max_tokens, openai_api_version=self.openai_client._api_version)
     
     def get_embedding_model(self):
-        return OpenAIEmbeddings(deployment=self.embedding_model, chunk_size=1)
+        return AzureOpenAIEmbeddings(azure_deployment=self.embedding_model, chunk_size=1, validate_base_url=False)
     
     def get_chat_completion_with_functions(self, messages: List[dict], functions: List[dict], function_call: str="auto"):
-        return openai.ChatCompletion.create(
+        return self.openai_client.chat.completions.create(
             deployment_id=self.llm_model,
             messages=messages,
             functions=functions,
@@ -39,7 +35,7 @@ class LLMHelper:
             )
         
     def get_chat_completion(self, messages: List[dict]):
-        return openai.ChatCompletion.create(
+        return self.openai_client.chat.completions.create(
             deployment_id=self.llm_model,
             messages=messages,
             )
