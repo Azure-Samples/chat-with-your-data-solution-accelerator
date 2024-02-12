04fd49b3b9423b33056cb2039d3add3d2fc6530c
Francia Riesco <friesco@microsoft.com>
2024-02-12 14:41:25 -0500

    update to function type

--- code/batch/function_app.py
+++ code/batch/function_app.py
@@ -5,7 +5,7 @@ from BatchStartProcessing import bp_batch_start_processing
 from GetConversationResponse import bp_get_conversation_response
 
 app = func.FunctionApp(
-    http_auth_level=func.AuthLevel.ANONYMOUS
+    http_auth_level=func.AuthLevel.FUNCTION
 )  # change to ANONYMOUS for local debugging
 app.register_functions(bp_add_url_embeddings)
 app.register_functions(bp_batch_push_results)
