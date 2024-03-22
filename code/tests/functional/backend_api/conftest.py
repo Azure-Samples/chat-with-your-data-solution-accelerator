bc3d29d393f61c06a9692e29d774cbb0c59f4b25
Adam Dougal <adamdougal@microsoft.com>
2024-03-22 15:58:32 +0000

    Add fixture comments for the https mock server

--- code/tests/functional/backend_api/conftest.py
+++ code/tests/functional/backend_api/conftest.py
@@ -16,11 +16,19 @@ import app
 
 @pytest.fixture(scope="session")
 def ca():
+    """
+    This fixture is required to run the http mock server with SSL.
+    https://pytest-httpserver.readthedocs.io/en/latest/howto.html#running-an-https-server
+    """
     return trustme.CA()
 
 
 @pytest.fixture(scope="session")
 def httpserver_ssl_context(ca):
+    """
+    This fixture is required to run the http mock server with SSL.
+    https://pytest-httpserver.readthedocs.io/en/latest/howto.html#running-an-https-server
+    """
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     localhost_cert = ca.issue_cert("localhost")
     localhost_cert.configure_cert(context)
@@ -29,6 +37,10 @@ def httpserver_ssl_context(ca):
 
 @pytest.fixture(scope="session")
 def httpclient_ssl_context(ca):
+    """
+    This fixture is required to run the http mock server with SSL.
+    https://pytest-httpserver.readthedocs.io/en/latest/howto.html#running-an-https-server
+    """
     with ca.cert_pem.tempfile() as ca_temp_path:
         return ssl.create_default_context(cafile=ca_temp_path)
 
