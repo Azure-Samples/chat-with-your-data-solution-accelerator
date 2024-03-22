27278ed10d1513c82529177412b2466715c8f97d
Adam Dougal <adamdougal@microsoft.com>
2024-03-22 14:20:16 +0000

    Add test for failing downstream
    Required by https://github.com/Azure-Samples/chat-with-your-data-solution-accelerator/issues/420

--- code/tests/functional/backend_api/test_conversation_custom.py
+++ code/tests/functional/backend_api/test_conversation_custom.py
@@ -1,5 +1,6 @@
 import json
 import pytest
+from pytest_httpserver import HTTPServer
 import requests
 
 from tests.functional.backend_api.app_config import AppConfig
@@ -46,3 +47,35 @@ def test_post(app_url: str, app_config: AppConfig):
         "object": "response.object",
     }
     assert response.headers["Content-Type"] == "application/json"
+
+
+def test_post_returns_error_when_downstream_fails(
+    app_url: str, app_config: AppConfig, httpserver: HTTPServer
+):
+    # given
+    httpserver.clear_all_handlers()  # Clear default successful responses
+
+    httpserver.expect_request(
+        f"/openai/deployments/{app_config.get('AZURE_OPENAI_EMBEDDING_MODEL')}/embeddings",
+        method="POST",
+    ).respond_with_json({}, status=500)
+
+    # when
+    response = requests.post(
+        f"{app_url}/api/conversation/custom",
+        json={
+            "conversation_id": "123",
+            "messages": [
+                {"role": "user", "content": "Hello"},
+                {"role": "assistant", "content": "Hi, how can I help?"},
+                {"role": "user", "content": "What is the meaning of life?"},
+            ],
+        },
+    )
+
+    # then
+    assert response.status_code == 500
+    assert response.headers["Content-Type"] == "application/json"
+    assert json.loads(response.text) == {
+        "error": "Exception in /api/conversation/custom. See log for more details."
+    }
