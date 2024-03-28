fdb066bc6da5ad8bd6c91ec3080ebb4ccf307a99
Ross Smith <ross-p-smith@users.noreply.github.com>
2024-03-28 15:06:25 +0000

    Don't upload node_modules to App Service (#557)
    * Don't include the whole code directory
    
    * Pulled commands into scripts

--- code/frontend/vite.config.ts
+++ code/frontend/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: "../static",
+        outDir: "../dist/static",
         emptyOutDir: true,
         sourcemap: true
     },
