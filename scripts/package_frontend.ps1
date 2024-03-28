fcb1fdf5e3ee55653d9a9565d17a75e4a62f6a83
ross-p-smith <rosmith@microsoft.com>
2024-03-28 16:02:07 +0000

    Clean directory

--- scripts/package_frontend.ps1
+++ scripts/package_frontend.ps1
@@ -1,8 +1,12 @@
 mkdir dist -Force
+rm dist/* -r -Force
+
+# Python
 poetry export -o dist/requirements.txt
 cp app.py dist -Force
 cp backend dist -r -Force
 
+# Node
 cd frontend
 npm install
 npm run build
