d215a5e79966530cf8c7163cf19b6e79f3d92261
Maksym Tsapenko <maksym@Maksyms-MacBook-Pro.local>
2024-04-05 21:46:26 +0300

    add json document loader

--- code/backend/batch/utilities/helpers/ConfigHelper.py
+++ code/backend/batch/utilities/helpers/ConfigHelper.py
@@ -34,7 +34,7 @@ class Config:
         )
 
     def get_available_document_types(self):
-        return ["txt", "pdf", "url", "html", "md", "jpeg", "jpg", "png", "docx"]
+        return ["txt", "pdf", "url", "html", "md", "jpeg", "jpg", "png", "docx", "json"]
 
     def get_available_chunking_strategies(self):
         return [c.value for c in ChunkingStrategy]
