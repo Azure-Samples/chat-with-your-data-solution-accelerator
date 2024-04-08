3a1ffab896e3d36d61442bdfbf8d21af51aa4d0e
Maksym Tsapenko <maksym@Maksyms-MacBook-Pro.local>
2024-04-08 11:09:35 +0300

    rewrite json loader

--- code/backend/batch/utilities/document_loading/Json.py
+++ code/backend/batch/utilities/document_loading/Json.py
@@ -1,7 +1,9 @@
+import requests
+import tempfile
+
 from typing import List
-import re
 from langchain.docstore.document import Document
-from langchain_community.document_loaders import JSONLoader
+from langchain_community.document_loaders.json_loader import JSONLoader
 from .DocumentLoadingBase import DocumentLoadingBase
 from ..common.SourceDocument import SourceDocument
 
@@ -11,14 +13,13 @@ class JsonDocumentLoading(DocumentLoadingBase):
         super().__init__()
 
     def load(self, document_url: str) -> List[SourceDocument]:
-        documents: List[Document] = JSONLoader(document_url).load()
+        response = requests.get(document_url)
+        temp = tempfile.NamedTemporaryFile()
+        with open(temp.name, 'w') as file:
+            file.write(response.text)
+
+        documents: List[Document] = JSONLoader(file_path=temp.name, jq_schema=".pages[]", text_content=False).load()
         for document in documents:
-            document.page_content = re.sub("\n{3,}", "\n\n", document.page_content)
-            # Remove half non-ascii character from start/end of doc content
-            pattern = re.compile(
-                r"[\x00-\x1f\x7f\u0080-\u00a0\u2000-\u3000\ufff0-\uffff]"
-            )
-            document.page_content = re.sub(pattern, "", document.page_content)
             if document.page_content == "":
                 documents.remove(document)
         source_documents: List[SourceDocument] = [
