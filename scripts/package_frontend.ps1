205efb4a19ee6db069f80d134126d6471619fdb0
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2024-04-11 10:27:39 +0000

    Remove poetry install

--- scripts/package_frontend.ps1
+++ scripts/package_frontend.ps1
@@ -2,7 +2,6 @@ mkdir dist -Force
 rm dist/* -r -Force
 
 # Python
-poetry install
 poetry export -o dist/requirements.txt
 cp *.py dist -Force
 cp backend dist -r -Force
--- scripts/package_frontend.sh
+++ scripts/package_frontend.sh
@@ -3,7 +3,6 @@ set -eou pipefail
 
 mkdir -p dist
 rm -rf dist/*
-poetry install
 poetry export -o dist/requirements.txt
 cp *.py dist
 cp -r backend dist
