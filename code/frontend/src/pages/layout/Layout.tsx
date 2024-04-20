9e8dddbf2321897cdf5778b4f398b42ca8bfc8c7
bl3d <mr.brentlewis@gmail.com>
2024-04-20 14:12:38 -0400

    tooltip in for header links

--- code/frontend/src/pages/layout/Layout.tsx
+++ code/frontend/src/pages/layout/Layout.tsx
@@ -9,6 +9,7 @@ import {
   ArrowBidirectionalUpDown16Filled
 } from "@fluentui/react-icons";
 import { Dialog, Stack, TextField } from "@fluentui/react";
+import { Tooltip } from "@fluentui/react-components";
 import { useEffect, useState } from "react";
 
 const Layout = () => {
@@ -42,6 +43,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Stack horizontal verticalAlign="center">
+
                         <Link to="/" className={styles.headerTitleContainer}>
                             <img
                             src="../../src/assets/logo.svg"
@@ -50,25 +52,49 @@ const Layout = () => {
                             />
                             <h3 className={styles.headerTitle}>Pronto</h3>
                         </Link>
+
                         <div className={styles.mainNavLinks}>
-                          <div className={styles.addSourceButtonContainer}>
-                            <a href="https://rsta4xey-test-website-6eg6fe2yksguu-admin.azurewebsites.net/Ingest_Data" target="_blank" rel="noopener noreferrer">
-                              {/* <NoteAdd16Filled className={styles.addSourceButton} /> */}
+                          <Tooltip content={{ children:
+                            // ↓ this is where you form the tooltip content
+                            <div className={styles.dataSourceToolTipInner}>
+                              <span>Add a data source</span>
+                            </div>,
+                            className: styles.dataSourceToolTip
+                          }} relationship="label" positioning={'below'}>
+                            {/* ↓ this is where the hoverable content goes */}
+                            <div className={styles.addSourceButtonContainer}>
+                              <a href="https://rsta4xey-test-website-6eg6fe2yksguu-admin.azurewebsites.net/Ingest_Data" target="_blank" rel="noopener noreferrer">
+                                {/* <NoteAdd16Filled className={styles.addSourceButton} /> */}
+                                <img
+                                  src="../../addSourceIcon.png"
+                                  className={styles.addSourceButton}
+                                  aria-hidden="true"
+                                  />
+                              </a>
+                            </div>
+                          </Tooltip>
+
+
+                          <Tooltip content={{ children:
+                            // ↓ this is where you form the tooltip content
+                            <div className={styles.shareToolTipInner}>
+                              <span>Share Pronto</span>
+                              {/* <span>Link copied to clipboard!</span> */}
+                            </div>,
+                            className: styles.shareToolTip
+                          }} relationship="label" positioning={'below'}>
+                            {/* ↓ this is where the hoverable content goes */}
+                            <div className={styles.shareButtonContainer} role="button" tabIndex={0} aria-label="Share" onClick={handleShareClick} onKeyDown={e => e.key === "Enter" || e.key === " " ? handleShareClick() : null}>
+                              {/* <ShareAndroid16Filled className={styles.shareButton} /> */}
                               <img
-                                src="../../addSourceIcon.png"
-                                className={styles.addSourceButton}
+                                src="../../shareLinkIcon.png"
+                                className={styles.shareButton}
                                 aria-hidden="true"
                                 />
-                            </a>
-                          </div>
-                          <div className={styles.shareButtonContainer} role="button" tabIndex={0} aria-label="Share" onClick={handleShareClick} onKeyDown={e => e.key === "Enter" || e.key === " " ? handleShareClick() : null}>
-                            {/* <ShareAndroid16Filled className={styles.shareButton} /> */}
-                            <img
-                              src="../../shareLinkIcon.png"
-                              className={styles.shareButton}
-                              aria-hidden="true"
-                              />
-                          </div>
+                            </div>
+                          </Tooltip>
+
+
                           <div className={`${styles.userMenuContainer} ${styles.disabled}`}>
                             <div className={styles.userMenuBtn}>
                               <span>Eddie Hoover</span>
@@ -76,7 +102,9 @@ const Layout = () => {
                             </div>
                             {/* <div>Future menu content</div> */}
                           </div>
+
                         </div>
+
                     </Stack>
                 </div>
             </header>
