c6bdbcc07d6c8455590c274f1607b85afb2ed88d
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2024-04-25 10:27:02 +0000

    Make orchestration async

--- code/tests/test_GetConversationResponse.py
+++ code/tests/test_GetConversationResponse.py
@@ -1,6 +1,7 @@
 import sys
 import os
-from unittest.mock import patch, Mock, ANY
+from unittest.mock import AsyncMock, patch, Mock, ANY
+import pytest
 import json
 
 sys.path.append(os.path.join(os.path.dirname(sys.path[0]), "backend", "batch"))
@@ -12,7 +13,8 @@ from backend.batch.GetConversationResponse import (  # noqa: E402
 
 @patch("backend.batch.GetConversationResponse.ConfigHelper")
 @patch("backend.batch.GetConversationResponse.Orchestrator")
-def test_get_conversation_response(mock_create_message_orchestrator, _):
+@pytest.mark.asyncio
+async def test_get_conversation_response(mock_create_message_orchestrator, _):
     mock_http_request = Mock()
     request_json = {
         "messages": [
@@ -24,14 +26,16 @@ def test_get_conversation_response(mock_create_message_orchestrator, _):
     }
     mock_http_request.get_json.return_value = request_json
 
-    mock_message_orchestrator = Mock()
+    mock_message_orchestrator = AsyncMock()
     mock_message_orchestrator.handle_message.return_value = [
         "You don't have any meetings today"
     ]
 
     mock_create_message_orchestrator.return_value = mock_message_orchestrator
 
-    response = get_conversation_response.build().get_user_function()(mock_http_request)
+    response = await get_conversation_response.build().get_user_function()(
+        mock_http_request
+    )
 
     assert response.status_code == 200
 
