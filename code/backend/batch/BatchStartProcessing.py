f6d48dd458f771d9878a08994d90e87029a3dafb
Bhavana Hindupur <bhavana.hindupur@gmail.com>
2024-04-30 12:30:54 +0000

    Simplify BatchStartProcessing function method.

--- code/backend/batch/BatchStartProcessing.py
+++ code/backend/batch/BatchStartProcessing.py
@@ -20,12 +20,7 @@ def batch_start_processing(req: func.HttpRequest) -> func.HttpResponse:
     azure_blob_storage_client = AzureBlobStorageClient()
     # Get all files from Blob Storage
     files_data = azure_blob_storage_client.get_all_files()
-    # Filter out files that have already been processed
-    files_data = (
-        list(filter(lambda x: not x["embeddings_added"], files_data))
-        if req.params.get("process_all") != "true"
-        else files_data
-    )
+
     files_data = list(map(lambda x: {"filename": x["filename"]}, files_data))
 
     # Send a message to the queue for each file
