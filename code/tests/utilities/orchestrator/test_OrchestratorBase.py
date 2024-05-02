e5dc0b8c154cb3015cdbf9a6535fef1bd065effa
Chinedum Echeta <60179183+cecheta@users.noreply.github.com>
2024-05-02 11:50:49 +0100

    Add Semantic Kernel as orchestrator (#787)
    * Add Semantic Kernel as orchestrator
    
    * Add LLMHelper tests
    
    * Update docs
    
    * Minor changes

---
+++ code/tests/utilities/orchestrator/test_OrchestratorBase.py
@@ -0,0 +1,108 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+from backend.batch.utilities.orchestrator.OrchestratorBase import OrchestratorBase
+
+
+class MockOrchestrator(OrchestratorBase):
+    async def orchestrate(
+        self, user_message: str, chat_history: list[dict], **kwargs: dict
+    ):
+        return []
+
+
+@pytest.fixture(autouse=True)
+def config_mock():
+    with patch(
+        "backend.batch.utilities.orchestrator.OrchestratorBase.ConfigHelper"
+    ) as mock:
+        config = mock.get_active_config_or_default.return_value
+        yield config
+
+
+@pytest.fixture(autouse=True)
+def conversation_logger_mock():
+    with patch(
+        "backend.batch.utilities.orchestrator.OrchestratorBase.ConversationLogger"
+    ) as mock:
+        conversation_logger = mock.return_value
+        yield conversation_logger
+
+
+@pytest.fixture(autouse=True)
+def content_safety_checker_mock():
+    with patch(
+        "backend.batch.utilities.orchestrator.OrchestratorBase.ContentSafetyChecker"
+    ) as mock:
+        content_safety_checker = mock.return_value
+        yield content_safety_checker
+
+
+def test_call_content_safety_input_replace(content_safety_checker_mock: MagicMock):
+    # given
+    orchestrator = MockOrchestrator()
+    content_safety_checker_mock.validate_input_and_replace_if_harmful.return_value = (
+        "filtered user message"
+    )
+
+    # when
+    result = orchestrator.call_content_safety_input("user message")
+
+    # then
+    assert result == [
+        {
+            "role": "tool",
+            "content": '{"citations": [], "intent": "user message"}',
+            "end_turn": False,
+        },
+        {"role": "assistant", "content": "filtered user message", "end_turn": True},
+    ]
+
+
+def test_call_content_safety_input_no_replace(content_safety_checker_mock: MagicMock):
+    # given
+    orchestrator = MockOrchestrator()
+    content_safety_checker_mock.validate_input_and_replace_if_harmful.return_value = (
+        "user message"
+    )
+
+    # when
+    result = orchestrator.call_content_safety_input("user message")
+
+    # then
+    assert result is None
+
+
+def test_call_content_safety_output_replace(content_safety_checker_mock: MagicMock):
+    # given
+    orchestrator = MockOrchestrator()
+    content_safety_checker_mock.validate_output_and_replace_if_harmful.return_value = (
+        "filtered answer"
+    )
+
+    # when
+    result = orchestrator.call_content_safety_output("user message", "answer")
+
+    # then
+    assert result == [
+        {
+            "role": "tool",
+            "content": '{"citations": [], "intent": "user message"}',
+            "end_turn": False,
+        },
+        {"role": "assistant", "content": "filtered answer", "end_turn": True},
+    ]
+
+
+def test_call_content_safety_output_no_replace(content_safety_checker_mock: MagicMock):
+    # given
+    orchestrator = MockOrchestrator()
+    content_safety_checker_mock.validate_output_and_replace_if_harmful.return_value = (
+        "answer"
+    )
+
+    # when
+    result = orchestrator.call_content_safety_output("user message", "answer")
+
+    # then
+    assert result is None
---
+++ code/tests/utilities/orchestrator/test_SemanticKernel.py
@@ -0,0 +1,500 @@
+from unittest.mock import ANY, AsyncMock, MagicMock, call, patch
+
+import pytest
+from backend.batch.utilities.common.Answer import Answer
+from backend.batch.utilities.orchestrator.SemanticKernel import (
+    SemanticKernelOrchestrator,
+)
+from backend.batch.utilities.parser.OutputParserTool import OutputParserTool
+from semantic_kernel import Kernel
+from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
+from semantic_kernel.connectors.ai.open_ai.contents.azure_chat_message_content import (
+    AzureChatMessageContent,
+)
+from semantic_kernel.connectors.ai.open_ai.contents.function_call import FunctionCall
+from semantic_kernel.connectors.ai.open_ai.contents.tool_calls import ToolCall
+from semantic_kernel.connectors.ai.open_ai.prompt_execution_settings.azure_chat_prompt_execution_settings import (
+    AzureChatPromptExecutionSettings,
+)
+from semantic_kernel.contents.chat_role import ChatRole
+
+chat_message_default_content = AzureChatMessageContent(
+    content="mock-response",
+    role=ChatRole.ASSISTANT,
+    metadata={
+        "usage": MagicMock(
+            prompt_tokens=10,
+            completion_tokens=20,
+        )
+    },
+)
+
+
+@pytest.fixture(autouse=True)
+def llm_helper_mock():
+    with patch("backend.batch.utilities.orchestrator.SemanticKernel.LLMHelper") as mock:
+        llm_helper = mock.return_value
+
+        llm_helper.get_sk_chat_completion_service.return_value = AzureChatCompletion(
+            service_id="mock-service-id",
+            deployment_name="mock-deployment",
+            endpoint="https://mock-endpoint",
+            api_key="mock-api-key",
+        )
+
+        llm_helper.get_sk_service_settings.return_value = (
+            AzureChatPromptExecutionSettings(
+                service_id="mock-service-id", temperature=0, max_tokens=1000
+            )
+        )
+
+        yield llm_helper
+
+
+@pytest.fixture()
+def orchestrator():
+    with patch(
+        "backend.batch.utilities.orchestrator.SemanticKernel.OrchestratorBase.__init__"
+    ):
+        orchestrator = SemanticKernelOrchestrator()
+
+        orchestrator.tokens = {"prompt": 0, "completion": 0, "total": 0}
+
+        orchestrator.config = MagicMock()
+        orchestrator.config.prompts.enable_content_safety = True
+        orchestrator.config.prompts.enable_post_answering_prompt = True
+
+        orchestrator.call_content_safety_input = MagicMock(return_value=None)
+        orchestrator.call_content_safety_output = MagicMock(return_value=None)
+
+        orchestrator.output_parser = OutputParserTool()
+
+        yield orchestrator
+
+
+def test_kernel_init(orchestrator: SemanticKernelOrchestrator):
+    assert isinstance(orchestrator.kernel, Kernel)
+
+    assert orchestrator.kernel.services["mock-service-id"] is not None
+    assert orchestrator.kernel.services["mock-service-id"] is orchestrator.chat_service
+
+    assert orchestrator.kernel.plugins["PostAnswering"] is not None
+    assert (
+        orchestrator.kernel.plugins["PostAnswering"].functions["validate_answer"]
+        is not None
+    )
+
+
+@pytest.mark.asyncio
+async def test_content_safety_input(orchestrator: SemanticKernelOrchestrator):
+    # given
+    content_safety_response = [
+        {
+            "role": "tool",
+            "content": '{"citations": [], "intent": "bad question"}',
+            "end_turn": False,
+        },
+        {
+            "role": "assistant",
+            "content": "Content safety response input.",
+            "end_turn": True,
+        },
+    ]
+    orchestrator.call_content_safety_input = MagicMock(
+        return_value=content_safety_response
+    )
+
+    # when
+    response = await orchestrator.orchestrate("bad question", [])
+
+    # then
+    assert response == content_safety_response
+
+
+@pytest.mark.asyncio
+async def test_semantic_kernel_no_function_call(
+    orchestrator: SemanticKernelOrchestrator,
+):
+    # given
+    with patch.object(orchestrator, "kernel", wraps=orchestrator.kernel) as kernel_mock:
+        kernel_mock.invoke = AsyncMock()
+        kernel_mock.invoke.return_value.value = [chat_message_default_content]
+
+        # when
+        response = await orchestrator.orchestrate("question", [])
+
+    # then
+    assert response == [
+        {
+            "role": "tool",
+            "content": '{"citations": [], "intent": "question"}',
+            "end_turn": False,
+        },
+        {
+            "role": "assistant",
+            "content": "mock-response",
+            "end_turn": True,
+        },
+    ]
+
+    kernel_mock.invoke.assert_awaited_once_with(
+        function=ANY,
+        chat_history=ANY,
+        user_message="question",
+    )
+
+    assert orchestrator.tokens == {"prompt": 10, "completion": 20, "total": 30}
+
+
+@pytest.mark.asyncio
+async def test_chat_plugin_added(
+    orchestrator: SemanticKernelOrchestrator,
+):
+    # given
+    with patch.object(orchestrator, "kernel", wraps=orchestrator.kernel) as kernel_mock:
+        kernel_mock.invoke = AsyncMock()
+        kernel_mock.invoke.return_value.value = [chat_message_default_content]
+
+        # when
+        await orchestrator.orchestrate("question", [])
+
+    # then
+    assert kernel_mock.plugins["Chat"] is not None
+    assert kernel_mock.plugins["Chat"].functions["search_documents"] is not None
+    assert kernel_mock.plugins["Chat"].functions["text_processing"] is not None
+
+
+@pytest.mark.asyncio
+async def test_openai_functions_included(
+    orchestrator: SemanticKernelOrchestrator,
+):
+    # given
+    with patch.object(orchestrator, "kernel", wraps=orchestrator.kernel) as kernel_mock:
+        kernel_mock.invoke = AsyncMock()
+        kernel_mock.invoke.return_value.value = [chat_message_default_content]
+
+        # when
+        await orchestrator.orchestrate("question", [])
+
+    # then
+    tools = kernel_mock.add_function.call_args.kwargs["prompt_execution_settings"].tools
+
+    assert tools == [
+        {
+            "type": "function",
+            "function": {
+                "name": "Chat-search_documents",
+                "description": "Provide answers to any fact question coming from users.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "question": {
+                            "description": "A standalone question, converted from the chat history",
+                            "type": "string",
+                        }
+                    },
+                    "required": ["question"],
+                },
+            },
+        },
+        {
+            "type": "function",
+            "function": {
+                "name": "Chat-text_processing",
+                "description": "Useful when you want to apply a transformation on the text, like translate, summarize, rephrase and so on.",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "text": {
+                            "description": "The text to be processed",
+                            "type": "string",
+                        },
+                        "operation": {
+                            "description": "The operation to be performed on the text. Like Translate to Italian, Summarize, Paraphrase, etc. If a language is specified, return that as part of the operation. Preserve the operation name in the user language.",
+                            "type": "string",
+                        },
+                    },
+                    "required": ["text", "operation"],
+                },
+            },
+        },
+    ]
+
+
+@pytest.mark.asyncio
+async def test_semantic_kernel_text_processing(
+    orchestrator: SemanticKernelOrchestrator,
+):
+    # given
+    question = "question"
+
+    first_response = AzureChatMessageContent(
+        role=ChatRole.ASSISTANT,
+        tool_calls=[
+            ToolCall(
+                function=FunctionCall(
+                    name="Chat-text_processing",
+                    arguments='{"text": "mock-text", "operation": "mock-operation"}',
+                )
+            )
+        ],
+        metadata={
+            "usage": MagicMock(
+                prompt_tokens=100,
+                completion_tokens=200,
+            )
+        },
+    )
+
+    tool_response = Answer(
+        question=question,
+        answer="mock-text-processing-answer",
+        prompt_tokens=10,
+        completion_tokens=20,
+    )
+
+    with patch.object(orchestrator, "kernel", wraps=orchestrator.kernel) as kernel_mock:
+        kernel_mock.invoke = AsyncMock()
+        kernel_mock.invoke.side_effect = [
+            MagicMock(value=[first_response]),
+            MagicMock(value=tool_response),
+        ]
+
+        # when
+        response = await orchestrator.orchestrate(question, [])
+
+    # then
+    assert response == [
+        {
+            "role": "tool",
+            "content": '{"citations": [], "intent": "question"}',
+            "end_turn": False,
+        },
+        {
+            "role": "assistant",
+            "content": "mock-text-processing-answer",
+            "end_turn": True,
+        },
+    ]
+
+    assert kernel_mock.invoke.await_count == 2
+
+    kernel_mock.invoke.assert_awaited_with(
+        function=ANY,
+        text="mock-text",
+        operation="mock-operation",
+    )
+
+    assert orchestrator.tokens == {"prompt": 110, "completion": 220, "total": 330}
+
+
+@pytest.mark.asyncio
+async def test_semantic_kernel_search_documents_post_answering_prompt(
+    orchestrator: SemanticKernelOrchestrator,
+):
+    # given
+    first_response = AzureChatMessageContent(
+        role=ChatRole.ASSISTANT,
+        tool_calls=[
+            ToolCall(
+                function=FunctionCall(
+                    name="Chat-search_documents",
+                    arguments='{"question": "mock-tool-question"}',
+                )
+            )
+        ],
+        metadata={
+            "usage": MagicMock(
+                prompt_tokens=100,
+                completion_tokens=200,
+            )
+        },
+    )
+
+    tool_response = Answer(
+        question="mock-tool-question",
+        answer="mock-search-documents-answer",
+        prompt_tokens=10,
+        completion_tokens=20,
+    )
+
+    post_answering_response = Answer(
+        question="mock-tool-question",
+        answer="mock-post-answering-response",
+        prompt_tokens=50,
+        completion_tokens=60,
+    )
+
+    with patch.object(orchestrator, "kernel", wraps=orchestrator.kernel) as kernel_mock:
+        kernel_mock.invoke = AsyncMock()
+        kernel_mock.invoke.side_effect = [
+            MagicMock(value=[first_response]),
+            MagicMock(value=tool_response),
+            MagicMock(value=post_answering_response),
+        ]
+
+        # when
+        response = await orchestrator.orchestrate("question", [])
+
+    # then
+    assert response == [
+        {
+            "role": "tool",
+            "content": '{"citations": [], "intent": "mock-tool-question"}',
+            "end_turn": False,
+        },
+        {
+            "role": "assistant",
+            "content": "mock-post-answering-response",
+            "end_turn": True,
+        },
+    ]
+
+    assert kernel_mock.invoke.await_count == 3
+
+    kernel_mock.invoke.assert_has_awaits(
+        [
+            call(
+                function=ANY,
+                question="mock-tool-question",
+            ),
+            call(
+                function_name="validate_answer",
+                plugin_name="PostAnswering",
+                answer=tool_response,
+            ),
+        ]
+    )
+
+    assert orchestrator.tokens == {"prompt": 160, "completion": 280, "total": 440}
+
+
+@pytest.mark.asyncio
+async def test_semantic_kernel_search_documents_without_post_answering_prompt(
+    orchestrator: SemanticKernelOrchestrator,
+):
+    # given
+    orchestrator.config.prompts.enable_post_answering_prompt = False
+
+    first_response = AzureChatMessageContent(
+        role=ChatRole.ASSISTANT,
+        tool_calls=[
+            ToolCall(
+                function=FunctionCall(
+                    name="Chat-search_documents",
+                    arguments='{"question": "mock-tool-question"}',
+                )
+            )
+        ],
+        metadata={
+            "usage": MagicMock(
+                prompt_tokens=100,
+                completion_tokens=200,
+            )
+        },
+    )
+
+    tool_response = Answer(
+        question="mock-tool-question",
+        answer="mock-search-documents-answer",
+        prompt_tokens=10,
+        completion_tokens=20,
+    )
+
+    with patch.object(orchestrator, "kernel", wraps=orchestrator.kernel) as kernel_mock:
+        kernel_mock.invoke = AsyncMock()
+        kernel_mock.invoke.side_effect = [
+            MagicMock(value=[first_response]),
+            MagicMock(value=tool_response),
+        ]
+
+        # when
+        response = await orchestrator.orchestrate("question", [])
+
+    # then
+    assert response == [
+        {
+            "role": "tool",
+            "content": '{"citations": [], "intent": "mock-tool-question"}',
+            "end_turn": False,
+        },
+        {
+            "role": "assistant",
+            "content": "mock-search-documents-answer",
+            "end_turn": True,
+        },
+    ]
+
+    assert kernel_mock.invoke.await_count == 2
+
+    kernel_mock.invoke.assert_awaited_with(
+        function=ANY,
+        question="mock-tool-question",
+    )
+
+    assert orchestrator.tokens == {"prompt": 110, "completion": 220, "total": 330}
+
+
+@pytest.mark.asyncio
+async def test_chat_history_included(
+    orchestrator: SemanticKernelOrchestrator,
+):
+    # given
+    chat_history = [
+        {"role": "user", "content": "Hello"},
+        {"role": "assistant", "content": "Hi, how can I help you today?"},
+    ]
+
+    with patch.object(orchestrator, "kernel", wraps=orchestrator.kernel) as kernel_mock:
+        kernel_mock.invoke = AsyncMock()
+        kernel_mock.invoke.return_value.value = [chat_message_default_content]
+
+        # when
+        await orchestrator.orchestrate("question", chat_history)
+
+    # then
+    chat_history = kernel_mock.invoke.call_args.kwargs["chat_history"]
+    messages = chat_history.messages
+
+    assert len(messages) == 3
+    assert messages[0].role == ChatRole.SYSTEM
+
+    assert messages[1].role == ChatRole.USER
+    assert messages[1].content == "Hello"
+
+    assert messages[2].role == ChatRole.ASSISTANT
+    assert messages[2].content == "Hi, how can I help you today?"
+
+
+@pytest.mark.asyncio
+async def test_content_safety_output(orchestrator: SemanticKernelOrchestrator):
+    # given
+    chat_message_content = AzureChatMessageContent(
+        content="bad-response",
+        role=ChatRole.ASSISTANT,
+        metadata={
+            "usage": MagicMock(
+                prompt_tokens=10,
+                completion_tokens=20,
+            )
+        },
+    )
+
+    content_safety_response = [
+        {
+            "role": "assistant",
+            "content": "Content safety response output.",
+            "end_turn": True,
+        },
+    ]
+    orchestrator.call_content_safety_output.return_value = content_safety_response
+
+    with patch.object(orchestrator, "kernel", wraps=orchestrator.kernel) as kernel_mock:
+        kernel_mock.invoke = AsyncMock()
+        kernel_mock.invoke.return_value.value = [chat_message_content]
+
+        # when
+        response = await orchestrator.orchestrate("question", [])
+
+    # then
+    assert response == content_safety_response
