ac0f10bc09a3894db4ac467ebbb108142f087b14
Komal Grover <komalgrover@microsoft.com>
2024-05-07 12:48:53 +0000

    type declaration for env_helper

--- code/backend/batch/utilities/helpers/embedders/EmbedderFactory.py
+++ code/backend/batch/utilities/helpers/embedders/EmbedderFactory.py
@@ -1,3 +1,4 @@
+from ..EnvHelper import EnvHelper
 from ..AzureBlobStorageClient import AzureBlobStorageClient
 from .PushEmbedder import PushEmbedder
 from .IntegratedVectorizationEmbedder import (
@@ -7,7 +8,7 @@ from .IntegratedVectorizationEmbedder import (
 
 class EmbedderFactory:
     @staticmethod
-    def create(env_helper):
+    def create(env_helper: EnvHelper):
         if env_helper.AZURE_SEARCH_USE_INTEGRATED_VECTORIZATION:
             return IntegratedVectorizationEmbedder(env_helper)
         else:
