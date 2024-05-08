e755928cbbe43a77e63af1b665f72bb5faed6b7a
Liam Moat <contact@liammoat.com>
2024-05-08 18:02:02 +0100

    fix: update local port to use 5050 (#847)
    Co-authored-by: frtibble <32080479+frtibble@users.noreply.github.com>

--- code/frontend/vite.config.ts
+++ code/frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://127.0.0.1:5000",
+                target: "http://127.0.0.1:5050",
                 changeOrigin: true,
                 secure: false
             }
