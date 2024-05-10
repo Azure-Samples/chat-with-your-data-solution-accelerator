93afe41a1fb3dbedc54ec125ec18f940b3768a33
Komal Grover <komalgrover@microsoft.com>
2024-05-10 15:42:05 +0000

    tests: fix

--- code/tests/functional/backend_api/conftest.py
+++ code/tests/functional/backend_api/conftest.py
@@ -55,6 +55,8 @@ def setup_default_mocking(httpserver: HTTPServer, app_config: AppConfig):
         }
     )
 
+    prime_search_to_trigger_creation_of_index(httpserver, app_config)
+
     httpserver.expect_request(
         "/indexes",
         method="POST",
@@ -145,3 +147,19 @@ def setup_default_mocking(httpserver: HTTPServer, app_config: AppConfig):
     yield
 
     httpserver.check()
+
+
+def prime_search_to_trigger_creation_of_index(
+    httpserver: HTTPServer, app_config: AppConfig
+):
+    # first request should return no indexes
+    httpserver.expect_oneshot_request(
+        "/indexes",
+        method="GET",
+    ).respond_with_json({"value": []})
+
+    # second request should return the index as it will have been "created"
+    httpserver.expect_request(
+        "/indexes",
+        method="GET",
+    ).respond_with_json({"value": [{"name": app_config.get("AZURE_SEARCH_INDEX")}]})
