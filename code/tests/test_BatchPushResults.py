4740dacb14aedd7d7e5c2865b59886c745055763
Liam Moat <liam.moat@microsoft.com>
2024-05-13 19:19:59 +0100

    tests: initial tests for batch_push_results control logic

--- code/tests/test_BatchPushResults.py
+++ code/tests/test_BatchPushResults.py
@@ -41,10 +41,61 @@ def test_get_file_name_from_message_no_filename():
     assert file_name == "test_filename.md"
 
 
+def test_batch_push_results_with_unhandled_event_type():
+    mock_queue_message = QueueMessage(
+        body='{"eventType": "Microsoft.Storage.BlobUpdated"}'
+    )
+
+    with pytest.raises(NotImplementedError):
+        batch_push_results.build().get_user_function()(mock_queue_message)
+
+
+@patch("backend.batch.BatchPushResults._process_document_created_event")
+def test_batch_push_results_with_blob_created_event(
+    mock_process_document_created_event,
+):
+    mock_queue_message = QueueMessage(
+        body='{"eventType": "Microsoft.Storage.BlobCreated", "filename": "test/test/test_filename.md"}'
+    )
+
+    batch_push_results.build().get_user_function()(mock_queue_message)
+
+    expected_message_body = json.loads(mock_queue_message.get_body().decode("utf-8"))
+    mock_process_document_created_event.assert_called_once_with(expected_message_body)
+
+
+@patch("backend.batch.BatchPushResults._process_document_created_event")
+def test_batch_push_results_with_no_event(mock_process_document_created_event):
+    mock_queue_message = QueueMessage(
+        body='{"data": { "url": "test/test/test_filename.md"} }'
+    )
+
+    batch_push_results.build().get_user_function()(mock_queue_message)
+
+    expected_message_body = json.loads(mock_queue_message.get_body().decode("utf-8"))
+    mock_process_document_created_event.assert_called_once_with(expected_message_body)
+
+
+@patch("backend.batch.BatchPushResults._process_document_deleted_event")
+def test_batch_push_results_with_blob_deleted_event(
+    mock_process_document_deleted_event,
+):
+    mock_queue_message = QueueMessage(
+        body='{"eventType": "Microsoft.Storage.BlobDeleted", "filename": "test/test/test_filename.md"}'
+    )
+
+    batch_push_results.build().get_user_function()(mock_queue_message)
+
+    expected_message_body = json.loads(mock_queue_message.get_body().decode("utf-8"))
+    mock_process_document_deleted_event.assert_called_once_with(expected_message_body)
+
+
 @patch("backend.batch.BatchPushResults.EnvHelper")
 @patch("backend.batch.BatchPushResults.AzureBlobStorageClient")
 def test_batch_push_results(
-    mock_azure_blob_storage_client, mock_env_helper, get_processor_handler_mock
+    mock_azure_blob_storage_client,
+    mock_env_helper,
+    get_processor_handler_mock: batch_push_results,
 ):
     mock_queue_message = QueueMessage(
         body='{"message": "test message", "filename": "test/test/test_filename.md"}'
