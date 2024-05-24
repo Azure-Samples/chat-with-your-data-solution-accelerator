ba8503e27059d1115561a0f5ba4d12b893438935
Martyna Marcinkowska <martyna.marcinkowska@onet.pl>
2024-05-24 15:58:39 +0000

    Clean up prompt flow orchestator structure

--- code/backend/batch/utilities/orchestrator/prompt_flow.py
+++ code/backend/batch/utilities/orchestrator/prompt_flow.py
@@ -10,6 +10,15 @@ logger = logging.getLogger(__name__)
 
 
 class PromptFlowOrchestrator(OrchestratorBase):
+    def __init__(self) -> None:
+        super().__init__()
+
+        # Set the URL and API key for the Prompt Flow endpoint
+        # This could potentially be moved to LLM Helper
+        # self.env_helper = EnvHelper()
+        self.url = '<prompt-flow-host>/score' # self.url = self.env_helper.PROMPT_FLOW_URL
+        self.api_key = ''  # self.api_key = self.env_helper.PROMPT_FLOW_API_KEY
+
     async def orchestrate(
         self, user_message: str, chat_history: List[dict], **kwargs: dict
     ) -> list[dict]:
@@ -26,41 +35,31 @@ class PromptFlowOrchestrator(OrchestratorBase):
 
         body = str.encode(json.dumps(data))
 
-        url = '<prompt-flow-host>/score' # TODO
-        api_key = ''  # TODO
-        if not api_key:
-            raise Exception("A key should be provided to invoke the endpoint")
-
         # The azureml-model-deployment header will force the request to go to a specific deployment.
         # Remove this header to have the request observe the endpoint traffic rules
         headers = {
             'Content-Type':'application/json',
-            'Authorization':('Bearer '+ api_key),
-            'azureml-model-deployment': '<name-aml-model-deployment>'
+            'Authorization':('Bearer '+ self.api_key),
+            'azureml-model-deployment': '<name-aml-model-deployment>' # TODO
         }
 
-        req = urllib.request.Request(url, body, headers)
+        req = urllib.request.Request(self.url, body, headers)
 
         try:
             response = urllib.request.urlopen(req)
-
             result = response.read()
-
-            # Debugging
-            print(result)
+            logger.debug(result)
         except urllib.error.HTTPError as error:
-            print("The request failed with status code: " + str(error.code))
-
-            # Debugging
-            print(error.info())
-            print(error.read().decode("utf8", 'ignore'))
+            logger.error("The request failed with status code: %s", str(error.code))
+            logger.error(error.info())
+            logger.error(error.read().decode("utf8", 'ignore'))
 
         # Transform response into answer
         answer = Answer(
             question=user_message,
             answer=result,
-            prompt_tokens=0, # TODO
-            completion_tokens=0, # TODO
+            prompt_tokens=0, # TODO: Does the response contain the number of tokens within metadata?
+            completion_tokens=0, # TODO: As above.
         )
 
         # Call Content Safety tool
