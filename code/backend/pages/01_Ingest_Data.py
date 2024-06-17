ad96992ef1a6c2739c4d144680b9aac39387599b
Rohini-Microsoft <v-rwalunj@microsoft.com>
2024-06-17 15:37:22 +0530

    added css file path

--- code/backend/pages/01_Ingest_Data.py
+++ code/backend/pages/01_Ingest_Data.py
@@ -19,21 +19,12 @@ st.set_page_config(
     layout="wide",
     menu_items=None,
 )
-mod_page_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            @media screen and (-ms-high-contrast: active), (forced-colors: active) {
-                section{
-                    border: 2px solid WindowText;padding: 10px;
-                    background-color: Window;
-                    color: WindowText;
-                }
-            }
-            </style>
-            """
-st.markdown(mod_page_style, unsafe_allow_html=True)
+def load_css(file_path):
+    with open(file_path) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+# Load the common CSS
+load_css("pages/Common.css")
 
 
 def reprocess_all():
