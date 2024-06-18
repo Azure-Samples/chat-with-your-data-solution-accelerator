33205a01ab8a8638563af60927477e8efbabb909
Adam Dougal <adamdougal@microsoft.com>
2024-06-18 16:30:45 +0100

    style: Reformat code to fix build
    It's currently failing with:
    ```
      code/backend/Admin.py:34:1: E302 expected 2 blank lines, found 1
      code/backend/Admin.py:39:1: E305 expected 2 blank lines after class or function definition, found 1
      code/backend/pages/01_Ingest_Data.py:23:1: E302 expected 2 blank lines, found 1
      code/backend/pages/01_Ingest_Data.py:28:1: E305 expected 2 blank lines after class or function definition, found 1
      code/backend/pages/01_Ingest_Data.py:30:1: E302 expected 2 blank lines, found 1
      code/backend/pages/02_Explore_Data.py:30:1: E302 expected 2 blank lines, found 1
      code/backend/pages/02_Explore_Data.py:35:1: E305 expected 2 blank lines after class or function definition, found 1
      code/backend/pages/03_Delete_Data.py:21:1: E302 expected 2 blank lines, found 1
      code/backend/pages/03_Delete_Data.py:26:1: E305 expected 2 blank lines after class or function definition, found 1
      code/backend/pages/04_Configuration.py:21:1: E302 expected 2 blank lines, found 1
      code/backend/pages/04_Configuration.py:26:1: E305 expected 2 blank lines after class or function definition, found 1
      code/tests/utilities/helpers/test_llm_helper.py:141:1: E302 expected 2 blank lines, found 1
      code/tests/utilities/helpers/test_llm_helper.py:144:5: E125 continuation line with same indent as next logical line
    ```

--- code/backend/batch/utilities/helpers/env_helper.py
+++ code/backend/batch/utilities/helpers/env_helper.py
@@ -227,9 +227,7 @@ class EnvHelper:
             "LOAD_CONFIG_FROM_BLOB_STORAGE"
         )
 
-        self.AZURE_ML_WORKSPACE_NAME = os.getenv(
-            "AZURE_ML_WORKSPACE_NAME", ""
-        )
+        self.AZURE_ML_WORKSPACE_NAME = os.getenv("AZURE_ML_WORKSPACE_NAME", "")
 
         self.PROMPT_FLOW_ENDPOINT_NAME = os.getenv("PROMPT_FLOW_ENDPOINT_NAME", "")
 
