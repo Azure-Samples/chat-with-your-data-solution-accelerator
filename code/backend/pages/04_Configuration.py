a8006aae669679fccd17c7a6372a52043347d3ae
Pavan Kumar <v-kupavan.microsoft.com>
2024-07-02 08:57:15 +0530

    Remove unused code

--- code/backend/pages/04_Configuration.py
+++ code/backend/pages/04_Configuration.py
@@ -48,14 +48,6 @@ if "post_answering_filter_message" not in st.session_state:
     st.session_state["post_answering_filter_message"] = (
         config.messages.post_answering_filter
     )
-if "error_429_message" not in st.session_state:
-    st.session_state["error_429_message"] = (
-        config.messages.error_429
-    )
-if "error_generic_message" not in st.session_state:
-    st.session_state["error_generic_message"] = (
-        config.messages.error_generic
-    )
 if "enable_content_safety" not in st.session_state:
     st.session_state["enable_content_safety"] = config.prompts.enable_content_safety
 if "example_documents" not in st.session_state:
@@ -387,12 +379,6 @@ try:
             "messages": {
                 "post_answering_filter": st.session_state[
                     "post_answering_filter_message"
-                ],
-                "error_429": st.session_state[
-                    "error_429_message"
-                ],
-                "error_generic": st.session_state[
-                    "error_generic_message"
                 ]
             },
             "example": {
