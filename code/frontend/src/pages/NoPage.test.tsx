282dc68eb20e60b8726507a4e223f22aa0431756
Kiran Siluveru <v-ksiluveru@microsoft.com>
2024-09-25 11:41:48 +0530

    Added test cases for NoPage Component

---
+++ code/frontend/src/pages/NoPage.test.tsx
@@ -0,0 +1,28 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import NoPage from "./NoPage";
+
+describe("NoPage.tsx", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test("renders NoPage Component heading", () => {
+    render(<NoPage />);
+    const headingElement = screen.getByRole("heading");
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test("renders NoPage Component with Correct Text", () => {
+    render(<NoPage />);
+    const ErrorElement = screen.getByText("404");
+    console.log("ErrorElement.textContent", ErrorElement.textContent);
+    expect(ErrorElement.textContent).toEqual("404");
+  });
+
+  test("renders NoPage Component heading Level 1", () => {
+    render(<NoPage />);
+    const headingElement = screen.getByRole("heading", { level: 1 });
+    expect(headingElement.tagName).toEqual("H1");
+  });
+});
