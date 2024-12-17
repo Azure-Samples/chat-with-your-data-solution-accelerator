ca48198b36a78470ce0079113ddd7e9dba6d03a0
UtkarshMishra-Microsoft <v-utkamishra@microsoft.com>
2024-12-17 15:09:01 +0530

    BUG-FIX-11704

--- code/frontend/vite.config.ts
+++ code/frontend/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
         host: true,
         proxy: {
             "/api": {
-                target: "http://127.0.0.1:5050",
+                target: "http://127.0.0.1:5000",
                 changeOrigin: true,
                 secure: false
             }
