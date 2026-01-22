33096cebb64902993244bc7e1ef8f0b25712bf2b
Ajit Padhi <v-padhiajit@microsoft.com>
2026-01-22 16:26:39 +0530

    udpated script

--- scripts/run_create_table_script.sh
+++ scripts/run_create_table_script.sh
@@ -6,11 +6,16 @@ baseUrl="$1"
 requirementFile="requirements.txt"
 requirementFileUrl=${baseUrl}"scripts/data_scripts/requirements.txt"
 resourceGroup="$2"
-serverName="$3"
+serverFqdn="$3"
 managedIdentityName="$4"
-vectorDimensions="$5"
+vectorDimensions="${5:-1536}"
+
+# Extract server name from FQDN (remove .postgres.database.azure.com suffix)
+serverName=$(echo $serverFqdn | sed 's/.postgres.database.azure.com//')
 
 echo "Script Started"
+echo "Server Name: $serverName"
+echo "Vector Dimensions: $vectorDimensions"
 
 # Get the public IP address of the machine running the script
 publicIp=$(curl -s https://api.ipify.org)
@@ -29,7 +34,7 @@ echo "Download completed"
 
 # Replace placeholders in the python script with actual values
 sed -i "s/managedIdentityName/${managedIdentityName}/g" "create_postgres_tables.py"
-sed -i "s/serverName/${serverName}/g" "create_postgres_tables.py"
+sed -i "s/serverName/${serverFqdn}/g" "create_postgres_tables.py"
 sed -i "s/vectorDimensions/${vectorDimensions}/g" "create_postgres_tables.py"
 
 pip install -r requirements.txt
